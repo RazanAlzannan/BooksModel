fdc406e8c0ccbe3806d7320241c0863f29eb3c24
Razan <razanalzannan@gmail.com>
2023-05-21 14:59:28 +0300

    Initial Commit

---
+++ BookModel/BookModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookModelApp.swift
+//  BookModel
+//
+//  Created by Razan Mohammed Alzannan on 01/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct BookModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
