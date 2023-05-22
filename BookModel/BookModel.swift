b81384dd90ce03612ccc6a66ca5986be641e2d43
Razan <razanalzannan@gmail.com>
2023-05-22 05:46:14 +0300

    Updated Version

---
+++ BookModel/BookModel.swift
@@ -0,0 +1,20 @@
+//
+//  BookModel.swift
+//  BookModel
+//
+//  Created by Razan Mohammed Alzannan on 01/11/1444 AH.
+//
+
+import SwiftUI
+
+struct BookModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookModel_Previews: PreviewProvider {
+    static var previews: some View {
+        BookModel()
+    }
+}
---
+++ BookModel/BookModelSwift.swift
@@ -0,0 +1,8 @@
+//
+//  BookModelSwift.swift
+//  BookModel
+//
+//  Created by Razan Mohammed Alzannan on 01/11/1444 AH.
+//
+
+import Foundation
